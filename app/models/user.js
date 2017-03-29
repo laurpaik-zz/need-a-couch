c5e4151d8adf758d8a4c2804755688d79b138bef
laurpaik <laurpaik@gmail.com>
2017-03-29 15:41:23 -0400

    Restricts links to authenticated users

--- app/models/user.js
+++ app/models/user.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   email: DS.attr('string'),
-  profile: DS.hasOne('profile'),
-  couchposts: DS.hasMany('couchpost')
+  profile: DS.hasOne('profile', { async: true }),
+  couchposts: DS.hasMany('couchpost', { async: true })
 });
