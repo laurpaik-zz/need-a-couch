568c2f53b9a0bd2d9dbfb5f014ced27b9bed7d32
laurpaik <laurpaik@gmail.com>
2017-03-29 12:23:32 -0400

    Changes input for couchFound
    - couchFound was not recognizing select, so I changed it to input
    type="boolean"

--- app/components/couchpost-list/edit.js
+++ app/components/couchpost-list/edit.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
-export default Ember.Component.extend({
 
+export default Ember.Component.extend({
   actions: {
     save () {
       let date = new Date (this.get('couchpost.dateNeeded'));
