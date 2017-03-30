bfcacfbcfab0f72cc9344e3a7d158356e2017353
laurpaik <laurpaik@gmail.com>
2017-03-30 18:05:14 -0400

    Clears sign-up form on sign-up success

--- app/routes/sign-up.js
+++ app/routes/sign-up.js
@@ -8,7 +8,16 @@ export default Ember.Route.extend({
     signUp (credentials) {
       this.get('auth').signUp(credentials)
       .then(() => this.get('auth').signIn(credentials))
-      .then(() => this.transitionTo('application'))
+      .then(() => {
+        credentials.password = null;
+        credentials.email = null;
+        credentials.passwordConfirmation = null;
+        credentials.givenName = null;
+        credentials.surname = null;
+        credentials.gender = null;
+        credentials.dob = null;
+        this.transitionTo('application');
+      })
       .then(() => {
         this.get('flashMessages')
         .success('Successfully signed-up! You have also been signed-in.');
